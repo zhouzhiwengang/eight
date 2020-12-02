bc76816431f791505688d994578d1aa0d997b73f
Zxyzzg <zhouzhiwengang@163.com>
2020-12-03 02:40:16 +0800

    房屋租赁系统前端初始化

---
+++ src/components/login.vue
@@ -0,0 +1,54 @@
+
+<template>
+  <div>
+    <input type="text" v-model="loginForm.username" placeholder="用户名"/>
+    <input type="text" v-model="loginForm.password" placeholder="密码"/>
+    <button @click="login">登录</button>
+  </div>
+</template>
+ 
+<script>
+import { mapMutations } from 'vuex';
+export default {
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      userToken: ''
+    };
+  },
+ 
+  methods: {
+    ...mapMutations(['changeLogin']),
+    login () {
+      let _this = this;
+      if (this.loginForm.username === '' || this.loginForm.password === '') {
+        alert('账号或密码不能为空');
+      } else {
+        this.$axios({
+          method: 'post',
+          url: '/common/login',
+          headers: {
+		     'Content-Type':'application/json;charset=utf-8' 
+	      },
+          data:{"username":_this.loginForm.username,"password":_this.loginForm.password},
+        }).then(res => {
+          console.log('123456');
+          console.log(res);
+          _this.userToken = res.token;
+          // 将用户token保存到vuex中
+          _this.changeLogin({ token: _this.userToken });
+          _this.$router.push('/home');
+          alert('登陆成功');
+        }).catch(error => {
+          alert('账号或密码错误');
+          console.log(error);
+        });
+      }
+    }
+  }
+};
+</script>
+
